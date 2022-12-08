afab3fe88e42802b79d2a0b76cdcd99fab5c84d8
Matias Cabrera <matgcabrera@gmail.com>
2022-12-08 17:40:02 -0300

    nuestros trabajos responsive almost complete

--- resources/views/contentWork.blade.php
+++ resources/views/contentWork.blade.php
@@ -13,146 +13,166 @@
 <!-- CONTENT -->
 
 <section class="section grey p40">
-    <div class="section-content flip-box-container">
+    <div class="section-content flip-box-container container">
 
-        <div class="flip-box-container-row">
+        <div class="flip-box-container-row row justify-content-evenly">
 
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/1.jpg')}}" alt="Coberturas FIC - 2020 y 2022" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Coberturas FIC - 2020 y 2022</h2>
+            <div class="col-lg-3 col-md-6 col-xs-12 pb-4 pb-lg-0">
+                <div class="flip-box  reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/1.jpg')}}" alt="Coberturas FIC - 2020 y 2022" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Coberturas FIC - 2020 y 2022</h2>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/2.jpg')}}" alt="Cortometraje Fiesta Calva - 2019" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Cortometraje Fiesta Calva - 2019</h2>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/3.jpg')}}" alt="Cortometraje Tuneles - 2022" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Cortometraje Tuneles - 2022</h2>
+            <div class="col-lg-3 col-md-6 col-xs-12 pb-4 pb-lg-0">
+                <div class="flip-box  reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/2.jpg')}}" alt="Cortometraje Fiesta Calva - 2019" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Cortometraje Fiesta Calva - 2019</h2>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div class="col-lg-3 col-md-6 col-xs-12 pb-4 pb-md-0">
 
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/4.jpg')}}" alt="Homenaje a Juan Peyrou - 2022" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Homenaje a Juan Peyrou - 2022</h2>
+                <div class="flip-box  reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/3.jpg')}}" alt="Cortometraje Tuneles - 2022" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Cortometraje Tuneles - 2022</h2>
+                        </div>
                     </div>
                 </div>
             </div>
-
+            <div class="col-lg-3 col-md-6 col-xs-12">
+                <div class="flip-box  reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/4.jpg')}}" alt="Homenaje a Juan Peyrou - 2022" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Homenaje a Juan Peyrou - 2022</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div class="flip-box-container-row reveal fade-bottom">
+        <div class="flip-box-container-row row justify-content-around">
 
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/5.jpg')}}" alt="Institucional Endémica - 2022" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Institucional Endémica - 2022</h2>
+            <div class="col-lg-3 col-md-6 col-xs-12 pb-4 pb-lg-0">
+                <div class="flip-box reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/5.jpg')}}" alt="Institucional Endémica - 2022" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Institucional Endémica - 2022</h2>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/6.jpg')}}" alt="Institucional Marca Sur - 2022" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Institucional Marca Sur - 2022</h2>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/7.jpg')}}" alt="Institucional Septum Inefop - 2019" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Institucional Septum Inefop - 2019</h2>
+            <div class="col-lg-3 col-md-6 col-xs-12 pb-4 pb-lg-0">
+                <div class="flip-box reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/6.jpg')}}" alt="Institucional Marca Sur - 2022" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Institucional Marca Sur - 2022</h2>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/8.jpg')}}" alt="Spot El Gran Disparate - 2020" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Spot El Gran Disparate - 2020</h2>
+            <div class="col-lg-3 col-md-6 col-xs-12 pb-4 pb-md-0">
+                <div class="flip-box reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/7.jpg')}}" alt="Institucional Septum Inefop - 2019" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Institucional Septum Inefop - 2019</h2>
+                        </div>
                     </div>
                 </div>
             </div>
 
+            <div class="col-lg-3 col-md-6 col-xs-12">
+                <div class="flip-box reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/8.jpg')}}" alt="Spot El Gran Disparate - 2020" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Spot El Gran Disparate - 2020</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div class="flip-box-container-row reveal fade-bottom">
+        <div class="flip-box-container-row row justify-content-around">
 
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/9.jpg')}}" alt="Spot Septum - 2021" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Spot Septum - 2021</h2>
+            <div class="col-lg-3 col-md-6 col-xs-12 pb-4 pb-lg-0">
+                <div class="flip-box reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/9.jpg')}}" alt="Spot Septum - 2021" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Spot Septum - 2021</h2>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/10.jpg')}}" alt="Toque Joaquín Peñagaricano - 2022" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Toque Joaquín Peñagaricano - 2022</h2>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/11.jpg')}}" alt="Tutoriales Ukelele Butiá TV - 2021" width="300px" style="border-radius: 20px">
-                    </div>
-                    <div class="flip-box-back">
-                        <h2>Tutoriales Ukelele Butiá TV - 2021</h2>
+            <div class="col-lg-3 col-md-6 col-xs-12 pb-4 pb-lg-0">
+                <div class="flip-box reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/10.jpg')}}" alt="Toque Joaquín Peñagaricano - 2022" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Toque Joaquín Peñagaricano - 2022</h2>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="flip-box">
-                <div class="flip-box-inner">
-                    <div class="flip-box-front">
-                        <img src="{{url('/images/nuestrosTrabajos/12.jpg')}}" alt="Videoclip Tranqui - 2020" width="300px" style="border-radius: 20px">
+            <div class="col-lg-3 col-md-6 col-xs-12 pb-4 pb-md-0">
+                <div class="flip-box reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/11.jpg')}}" alt="Tutoriales Ukelele Butiá TV - 2021" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Tutoriales Ukelele Butiá TV - 2021</h2>
+                        </div>
                     </div>
-                    <div class="flip-box-back">
-                        <h2>Videoclip Tranqui - 2020</h2>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 col-xs-12 pb-4 pb-lg-0">
+                <div class="flip-box reveal fade-bottom">
+                    <div class="flip-box-inner">
+                        <div class="flip-box-front">
+                            <img src="{{url('/images/nuestrosTrabajos/12.jpg')}}" alt="Videoclip Tranqui - 2020" width="300px" style="border-radius: 20px">
+                        </div>
+                        <div class="flip-box-back">
+                            <h2>Videoclip Tranqui - 2020</h2>
+                        </div>
                     </div>
                 </div>
             </div>
